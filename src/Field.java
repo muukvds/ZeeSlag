c9245f2e3eddfa3aa548da11ce791a33efec114d
muukvds <muukvds@users.noreply.github.com>
2017-10-16 11:25:06 +0200

    add shotAt method to Field

--- src/Field.java
+++ src/Field.java
@@ -2,10 +2,19 @@ import java.util.HashMap;
 
 public class Field {
 
-    HashMap<String,Square> fieldSquares = new HashMap<>();
+    private HashMap<String,Square> fieldSquares = new HashMap<>();
 
     public Field() {
 
+    }
+
+    public void shotAt(String coordinates)
+    {
+        Square square = fieldSquares.get(coordinates);
+        if (square != null)
+        {
+            square.setShotAt();
+        }
     }
 
 }
