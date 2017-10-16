e1f8cf407731f213e593fabfa672bb71601bb947
muukvds <muukvds@users.noreply.github.com>
2017-10-16 23:48:36 +0200

    Create Coordinates class to store two arraylist objects

---
+++ src/Coordinates.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Coordinates {
+
+    ArrayList<String> surroundingCoordinates;
+    ArrayList<String> Coordinates;
+
+    public ArrayList<String> getSurroundingCoordinates() {
+        return surroundingCoordinates;
+    }
+
+    public void setSurroundingCoordinates(ArrayList<String> surroundingCoordinates) {
+        this.surroundingCoordinates = surroundingCoordinates;
+    }
+
+    public ArrayList<String> getCoordinates() {
+        return Coordinates;
+    }
+
+    public void setCoordinates(ArrayList<String> coordinates) {
+        Coordinates = coordinates;
+    }
+}
