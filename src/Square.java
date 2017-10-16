c43fdde8cd53c410655f81d32ce55e6768d2281b
muukvds <muukvds@users.noreply.github.com>
2017-10-16 11:26:42 +0200

    add setters and print method to Squars class

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,36 @@
 public class Square {
+
+    private Ship ship;
+    private boolean shotAt = false;
+
+    public Square() {
+    }
+
+    public Square(Ship ship) {
+        this.ship = ship;
+    }
+
+    public void setShotAt() {
+        this.shotAt = true;
+    }
+
+    public String print() {
+        String symbol;
+
+        if (!shotAt) {
+            symbol = ".";
+        } else {
+
+            if (ship == null) {
+                symbol = "~";
+            } else {
+                if (ship.isDied()) {
+                    symbol = ship.getInitial();
+                } else {
+                    symbol = "*";
+                }
+            }
+        }
+        return symbol;
+    }
 }
