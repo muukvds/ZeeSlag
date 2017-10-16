c41a34c2914f6205431b6bfc0f1969017ef4cbaa
muukvds <muukvds@users.noreply.github.com>
2017-10-16 02:32:28 +0200

    add parameters, constructor and hit method to Ship class

--- src/Ship.java
+++ src/Ship.java
@@ -1,2 +1,23 @@
 public class Ship {
+
+    private String type;
+    private int length;
+    private int hits = 0;
+    private boolean died = false;
+
+    public Ship(String type, int length) {
+        this.type = type;
+        this.length = length;
+    }
+
+    public boolean isDied() {
+        return died;
+    }
+
+    public void hit() {
+        hits++;
+        if (hits >= length) {
+            died = true;
+        }
+    }
 }
